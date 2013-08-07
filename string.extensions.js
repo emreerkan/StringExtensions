f6d0c1e2ce5d1b4ddc004eab8432f82d293187c5
Emre Erkan <kara@karalamalar.net>
2013-08-07 13:01:40 +0300

    Fixed missing semicolon

--- string.extensions.js
+++ string.extensions.js
@@ -151,7 +151,7 @@ Number.prototype.toTime = function(compact) {
     second = this - (hour * 3600 + minute * 60),
     returnValue = [hour < 10 ? 0 : '', hour, ':', minute < 10 ? 0 : '', minute, ':', second < 10 ? 0 : '', second].join('');
   return compact ? returnValue.trimLeft('0').trimLeft(':').trimLeft('0').trimLeft(':').trimLeft('0') : returnValue;
-}
+};
 
 String.prototype.toTime = function(compact) {
     return parseInt(this, 10).toTime(compact);
